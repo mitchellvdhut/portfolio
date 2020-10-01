4a5dbd9d2164d6654d9680bb84193b10ff2ca210
karavaan <quint.van.zijl@ah.nl>
2020-10-01 11:35:42 +0200

    loading pdf from url

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Document } from 'react-pdf/dist/esm/entry.webpack';
+import { Page } from 'react-pdf';
 
 const contact = () => {
-    return <div className="contact">Contact</div>;
+    return (
+        <div className="contact">
+            <Document file="https://mitchellvdhut.com/cv.pdf">
+                <Page pageNumber={1} />
+            </Document>
+        </div>
+    );
 };
+
 export default contact;
