0e4c652f626a8662f9be913e159951959c72ed5a
karavaan <quint.van.zijl@ah.nl>
2020-10-02 22:37:28 +0200

    speedom

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Projects from '../projects/projects';
-import About from '../about/about';
-import Contact from '../contact/contact';
-import CV from '../cv/cv';
-import Project from '../projects/project/project';
-
+const About = React.lazy(() => import('../about/about'));
+const CV = React.lazy(() => import('../cv/cv'));
+const Contact = React.lazy(() => import('../contact/contact'));
+const Project = React.lazy(() => import('../projects/project/project'));
 const Router = () => {
     return (
-        <Switch>
-            <Route exact path="/" component={Projects} />
-            <Route path="/about-me" component={About} />
-            <Route path="/cv" component={CV} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/project/:test" component={Project} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+                <Route exact path="/" component={Projects} />
+                <Route path="/about-me" component={About} />
+                <Route path="/cv" component={CV} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/project/:test" component={Project} />
+            </Switch>
+        </Suspense>
     );
 };
 export default Router;
